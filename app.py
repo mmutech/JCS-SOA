1c373bba5dda3647dd42deac44a1b32191d792cb
Nuruddeen Sambo <sambonuruddeen@gmail.com>
2025-05-30 15:50:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ def export_soa():
 
 if __name__ == "__main__":
     with app.app_context():
-        app.run(debug=True)
+        #app.run(debug=True)
+        app.run(host='0.0.0.0', port=5000)
